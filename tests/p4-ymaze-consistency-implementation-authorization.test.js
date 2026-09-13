57b6ef03983d6f875d48f110224a53f159b6b98a
flesentine <chrisgmcc@gmail.com>
2026-09-13 16:31:36 -0700

    Update authorization lifecycle pins for hardened tooling

--- tests/p4-ymaze-consistency-implementation-authorization.test.js
+++ tests/p4-ymaze-consistency-implementation-authorization.test.js
@@ -75,8 +75,8 @@ const fixedLaterBlobs=new Map([
   [s.left_experiment,'08edc2d3ef9899820c04e900154f1b4bee2a5104'],
   [s.right_experiment,'217f94ec4ba61a61c7956baeac34abd19cf5eec8'],
   [s.neutral_experiment,'e58cf3f4f5c51168f2dc267af7a820bc5b875b90'],
-  [s.stage_A_runner,'af0fd2d91f67102c62243bef289bbfe20cf471d7'],
-  [s.stage_B_comparator,'8e8c1ecf1a98fb14fe2e83ce38be224d7739c11c']
+  [s.stage_A_runner,'fe3bff285290d0d612a5b9ab665e887c728e3d2e'],
+  [s.stage_B_comparator,'02333b7e49e0fcc78fa13850088511e482291f13']
 ]);
 const implementationPresent=fs.existsSync(path.join(root,s.fitted_model));
 if(implementationPresent){
